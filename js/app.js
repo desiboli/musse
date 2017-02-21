51c97f9906f871ff3da8deb2d1e7497eae8275bc
Mustafa Alshamaa <mustafa@bricco.se>
2017-02-21 09:32:25 +0100

    particles

--- js/app.js
+++ js/app.js
@@ -3,20 +3,26 @@
   'use strict';
 
   $(function () {
+    // Activate WOW.js
     new WOW().init();
 
     // Cache the Window object
     var $window = $(window);
+    // Get the window width
     var width = $window.width();
 
+    // Toggle class on click on mobile menu
     $( '.hamburger' ).click(function() {
       $( this ).toggleClass( 'is-active' );
     });
 
+    // Save offset and duraion values in var
     var offset = 220;
 		var duration = 800;
 
+    // Check window width
     if (width > 400) {
+      // Scroll to top fade in link
       $(window).scroll(function() {
   			if (jQuery(this).scrollTop() > offset) {
           $('a.top').fadeIn(duration);
@@ -26,34 +32,12 @@
   		});
     }
 
+    // Scroll to top animate
     $('a.top').click(function(event) {
 			event.preventDefault();
       $('html, body').animate({scrollTop: 0}, duration);
 			return false;
 		})
-
-    // jQuery
-    $('section').imagesLoaded( { background: '.item' }, function() {
-      // Parallax Backgrounds
-      // Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
-      $('.parallax-bg[data-type="background"]').each(function(){
-        var $bgobj = $(this); // assigning the object
-
-        $(window).scroll(function() {
-
-          // Scroll the background at var speed
-          // the yPos is a negative value because we're scrolling it UP!
-          var yPos = -($window.scrollTop() / $bgobj.data('speed') - 200);
-
-          // Put together our final background position
-          var coords = '50% '+ yPos + 'px';
-
-          // Move the background
-          $bgobj.css({ backgroundPosition: coords });
-
-        }); // end window scroll
-      });
-    });
 
     // Add smooth scrolling on all links inside the navbar
     $('a').on('click', function(event) {
@@ -76,6 +60,7 @@
       } // End if
     });
 
+    // Typed.js animate text
     $(".typed-first").typed({
       strings: ["Hello I'm a"],
       // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
@@ -105,16 +90,10 @@
       // call when done callback function
       callback: function() {
         showThis();
-      },
-      // starting callback function before each string
-      preStringTyped: function() {},
-      //callback for every typed string
-      onStringTyped: function() {},
-      // callback for reset
-      resetCallback: function() {}
+      }
     });
 
-    function showThis(){
+    function showThis() {
       $(".typed-second").typed({
         strings: ["Front-end developer", "footballer", "Real Madrid fan", "great guy!"],
         backDelay: 1500,
@@ -123,6 +102,29 @@
         loop: true,
       });
     }
+
+    // Parallax background effect.
+    // $('section').imagesLoaded( { background: '.item' }, function() {
+    //   // Parallax Backgrounds
+    //   // Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
+    //   $('.parallax-bg[data-type="background"]').each(function(){
+    //     var $bgobj = $(this); // assigning the object
+    //
+    //     $(window).scroll(function() {
+    //
+    //       // Scroll the background at var speed
+    //       // the yPos is a negative value because we're scrolling it UP!
+    //       var yPos = -($window.scrollTop() / $bgobj.data('speed') - 200);
+    //
+    //       // Put together our final background position
+    //       var coords = '50% '+ yPos + 'px';
+    //
+    //       // Move the background
+    //       $bgobj.css({ backgroundPosition: coords });
+    //
+    //     }); // end window scroll
+    //   });
+    // });
 
   });
 
@@ -142,123 +144,123 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 /* Otherwise just put the config content (json): */
 
-particlesJS('particles-js',
-
-  {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#DE3C4B"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.8,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 100
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#b61924",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
-    }
-  }
-
-);
+// particlesJS('particles-js',
+//
+//   {
+//     "particles": {
+//       "number": {
+//         "value": 80,
+//         "density": {
+//           "enable": true,
+//           "value_area": 800
+//         }
+//       },
+//       "color": {
+//         "value": "#DE3C4B"
+//       },
+//       "shape": {
+//         "type": "circle",
+//         "stroke": {
+//           "width": 0,
+//           "color": "#000000"
+//         },
+//         "polygon": {
+//           "nb_sides": 5
+//         },
+//         "image": {
+//           "src": "img/github.svg",
+//           "width": 100,
+//           "height": 100
+//         }
+//       },
+//       "opacity": {
+//         "value": 0.5,
+//         "random": false,
+//         "anim": {
+//           "enable": false,
+//           "speed": 1,
+//           "opacity_min": 0.1,
+//           "sync": false
+//         }
+//       },
+//       "size": {
+//         "value": 5,
+//         "random": true,
+//         "anim": {
+//           "enable": false,
+//           "speed": 40,
+//           "size_min": 0.1,
+//           "sync": false
+//         }
+//       },
+//       "line_linked": {
+//         "enable": true,
+//         "distance": 150,
+//         "color": "#ffffff",
+//         "opacity": 0.8,
+//         "width": 1
+//       },
+//       "move": {
+//         "enable": true,
+//         "speed": 6,
+//         "direction": "none",
+//         "random": false,
+//         "straight": false,
+//         "out_mode": "out",
+//         "attract": {
+//           "enable": false,
+//           "rotateX": 600,
+//           "rotateY": 1200
+//         }
+//       }
+//     },
+//     "interactivity": {
+//       "detect_on": "canvas",
+//       "events": {
+//         "onhover": {
+//           "enable": true,
+//           "mode": "repulse"
+//         },
+//         "onclick": {
+//           "enable": true,
+//           "mode": "push"
+//         },
+//         "resize": true
+//       },
+//       "modes": {
+//         "grab": {
+//           "distance": 400,
+//           "line_linked": {
+//             "opacity": 1
+//           }
+//         },
+//         "bubble": {
+//           "distance": 400,
+//           "size": 40,
+//           "duration": 2,
+//           "opacity": 8,
+//           "speed": 3
+//         },
+//         "repulse": {
+//           "distance": 100
+//         },
+//         "push": {
+//           "particles_nb": 4
+//         },
+//         "remove": {
+//           "particles_nb": 2
+//         }
+//       }
+//     },
+//     "retina_detect": true,
+//     "config_demo": {
+//       "hide_card": false,
+//       "background_color": "#b61924",
+//       "background_image": "",
+//       "background_position": "50% 50%",
+//       "background_repeat": "no-repeat",
+//       "background_size": "cover"
+//     }
+//   }
+//
+// );
--- js/particles.js
+++ js/particles.js
@@ -12,9 +12,9 @@
 
   $(function () {
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    // particlesJS.load('particles-js', 'particles.json', function() {
-    //   console.log('callback - particles.js config loaded');
-    // });
+    particlesJS.load('particles-js', 'particles.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
   });
 
 })(jQuery, window, document);
