83a38bbd01901ace252386a86224f777f3066663
Mustafa Alshamaa <mustafa@bricco.se>
2017-02-22 11:23:30 +0100

    cv button fix

--- js/particles.js
+++ js/particles.js
@@ -13,7 +13,7 @@
   $(function () {
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
     particlesJS.load('particles-js', 'particles.json', function() {
-      console.log('callback - particles.js config loaded');
+      //console.log('callback - particles.js config loaded');
     });
   });
 
